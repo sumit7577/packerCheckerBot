adb1bc09176a0212f5d2ff9f6bca11df0a3af1e0
sumit7577 <asmitkumarr082@gmail.com>
2021-10-11 00:21:49 +0530

    some more fix to code

--- main.py
+++ main.py
@@ -21,19 +21,14 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True,content_types=['document'])
 def get_apk(message):
     fileData = message.document.file_name
-    if(".apk" in fileData):
-        data = bot.get_file(fileData.file_id)
-        download_file = bot.download_file(data.file_path)
-        path = os.path.join(BASE_DIR,"file.apk")
-        print(path)
-        with open(path,mode="wb")as file:
-            file.write(download_file)
-        data = subprocess.run(["apkid",path],capture_output=True)
-        final_data = data.stdout.decode("utf-8")
-        bot.reply_to(message,final_data)
-    else:
-        bot.reply_to(message,"Please Provide Valid apk file")
-
+    reversed = fileData[::-1]
+    data = bot.get_file(fileData.file_id)
+    download_file = bot.download_file(data.file_path)
+    with open("file.apk",mode="wb")as file:
+        file.write(download_file)
+    data = subprocess.run(["apkid","file.apk"],capture_output=True)
+    final_data = data.stdout.decode("utf-8")
+    bot.reply_to(message,final_data)
 
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
